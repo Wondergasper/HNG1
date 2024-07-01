5db3e70e71104c61eb77d3cb81f50f18171192d6
gasper wonder <gasperwonder@gmail.com>
2024-07-01 18:19:43 +0100

    some changes

--- projectone/index.js
+++ projectone/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-require('dotenv').config();rs
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,13 +12,29 @@ app.get('/api/hello', async (req, res) => {
     try {
         // Get location information using ip-api.com
         const geoResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
+        
+        if (geoResponse.status !== 200 || !geoResponse.data.city) {
+            res.status(500).json({ error: 'Unable to fetch location information' });
+            return;
+        }
+
         const location = geoResponse.data.city || 'New York';
 
         // Get weather information using OpenWeatherMap
         const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-        const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`);
-        const temperature = weatherResponse.data.main.temp;
+        if (!apiKey) {
+            res.status(500).json({ error: 'Missing OpenWeatherMap API key' });
+            return;
+        }
 
+        const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`);
+        
+        if (weatherResponse.status !== 200 || !weatherResponse.data.main || weatherResponse.data.main.temp === undefined) {
+            res.status(500).json({ error: 'Unable to fetch weather information' });
+            return;
+        }
+
+        const temperature = weatherResponse.data.main.temp;
         const greeting = `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`;
 
         res.json({
